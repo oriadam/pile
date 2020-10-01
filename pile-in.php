f342772e1d80b0ca32fff193edd5ac5888c11395
Oria <oriadam@gmail.com>
2020-10-01 12:05:58 +0300

    Create pile-in.php

---
+++ pile-in.php
@@ -0,0 +1,25 @@
+<?php
+require('pile-config.php');
+header("Content-type: text/plain");
+$conf = pile_config();
+$fn = $conf['fn'];
+
+if ($_GET['remove_the_file'])
+{
+	unlink($fn);
+	die('file removed');
+}
+
+$o = [
+	'date' => date('c'),
+];
+
+if ($_POST)
+	$o['POST'] = $_POST;
+if ($_GET)
+	$o['GET'] = $_GET;
+    
+$str = pile_encode($o);
+$str = iconv("CP1255","UTF-8", $str) . ",";
+file_put_contents($fn,$str,FILE_APPEND);
+?>1
